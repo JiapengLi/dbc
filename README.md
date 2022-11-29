dd5b4b268d1644b3b76e530201a44f87b4e5675b
JiapengLi <mail@jiapeng.me>
2022-11-29 15:33:32 +0800

    update README, simplify excel template ready to generate service 02

--- README.md
+++ README.md
@@ -2,38 +2,80 @@
 
 After involved in Car OBD area, I get to know about DBC (DBC file is a proprietary format that describes the data over a CAN bus). But the whole DBC thing is a kind of mess, you can only get something to work, but indeed not all. So I decide to change it, the target of this project is to **open source a full functional DBC file to support ISO15031-4 and J1979DA**. 
 
+I hope to make it easy to design an OBD simulator for developers or even certification labs with the dbc file provided by this project.
+
 Let's go!
 
 ## Features
 
 - [ ] Support ISOTP message encoding and decoding (longer than 8)
-- [ ] Full support **ISO15031-5** & **J1979DA** 
+- [ ] Full support **ISO15031-5-2015** & **J1979DA-202203** 
   - [ ]  J1979DA `Supported IDs 0x00` for ISO15031-5 `Service 01 - 0A`
-- [ ] Mixed big and little endian
+- [ ] Mixed big endian and little endian encoding to make the dbc file more easier to read
+- [ ] Auto generate dbc file from excel template (it is possible to generate other format database with minimum development)
+- [ ] 
 
-With the help of this project, I hope to make it easy to design an OBD simulator for developer or even certification libraries.
+### Remark
+
+- Service 01 PID13 and PID1D conflicts, use different variable name
+- Service 01, 02 shares same PID definition, but service 02 need one extra `frame#` byte, which means DATA_x of Service 02 is 8 bits larger than Service 01
+- 
+
+
 
 ## DBC Specification
 
+![J1939 EEC1 EngSpeed RPM CAN DBC File Database Format Message Signal](https://img.jiapeng.me/CAN-DBC-File-Format-Explained-Intro-Basics_2.png)
 
+*Source: https://canlogger1000.csselectronics.com/img/CAN-DBC-File-Format-Explained-Intro-Basics_2.png*
 
-![File:Screenshot 2015-11-22 16.02.04.png](https://img.jiapeng.me/800px-Screenshot_2015-11-22_16.02.04.png)
+### Cons
 
+#### DBC Big Endian is Counterintuitive
 
+J1979DA defines value which is longer than 8 bits through big endianess, this makes it necessary to use big endianess in dbc file to make it parse data correctly. However, I don't like the way how dbc spec to use big endian data, it is counterintuitive. Let me try to explain it a little bit. 
 
-### Little Endian or Big Endian
+For an 8 bytes CAN message, ISO15031-5 defines it in a sequence like this `ISOTP HEADER, Service, PID, A, B, C, D, E`, and thus we can transform it to a very large data (64bits) like this:
 
+![image-20221129144157483](https://img.jiapeng.me/image-20221129144157483.png)
 
+You can see from above picture, the big endian format of a can message is not a true big endian, it is **big endian in byte sequence**, and **still little endian in bit sequence**. This makes big endian dbc SG_ hard to understand (and we can't by pass it).
 
+For example :
 
+| Field | Little Endian | Big Endian |
+| ----- | ------------- | ---------- |
+| PID   | `16|8@1+`     | `23|8@0+`  |
+| A     | `24|8@1+`     | `31|8@0+`  |
+| B     | `32|8@1+`     | `39|8@0+`  |
+| AB    | N/A           | `31|16@0+` |
+| BA    | `24|16@1+`    | N/A        |
 
+You can see, a field in `AB` format is must be defined with big endian.
 
+A real example, RPM dbc parser  looks like below:
 
+**J1979DA Description:**
 
+| SAE J1979/ ISO 15031-5 PID | Description | External test equipment SI (Metric) / English display | Data Byte | Min. Value | Max. Value     | Scaling/bit   | External test equipment SI (Metric) / English display | Comment                                                      |
+| -------------------------- | ----------- | ----------------------------------------------------- | --------- | ---------- | -------------- | ------------- | ----------------------------------------------------- | ------------------------------------------------------------ |
+| 0x0C                       | Engine RPM  | RPM                                                   | A,B       | 0 min-1    | 16383.75 min-1 | ¼ rpm per bit | RPM: xxxxx min-1                                      | Engine RPM shall display revolutions per minute of the engine crankshaft. |
 
-## Limitations
+**DBC SG_:**
 
-- Service 01 PID13 and PID1D conflicts, use different variable name
+```
+ SG_ RPM m12 : 31|16@0+ (0.25,0) [0|0] "rpm" TOOL
+```
+
+#### Limited Data Types
+
+DBC SG_ can support number based types like int / float (double) / enum.
+
+DBC SG_ doesn't support string or array (list) format.
+
+#### Fixed length message
+
+DBC BO_ expected a fixed length message? It is not good if so.
 
 
 
--- util/dbc.py
+++ util/dbc.py
@@ -5,6 +5,7 @@ import cantools
 from pprint import pprint
 
 import pandas as pd
+import numpy as np
 
 def fmt(msg):
     return msg + (64 - len(msg)) * b'\x00'
@@ -94,35 +95,100 @@ BU_:
 BO_ 2024 RX: 64 ECU
  SG_ RXLEN : 0|8@1+ (1,0) [0|0] "" TOOL
  SG_ SERVICE M : 8|4@1+ (1,0) [0|0] "" TOOL
- SG_ RXACK : 12|4@1+ (1,0) [0|0] "" TOOL
- SG_ S01_PID m1M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S02_PID m2M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S03_PID m3M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S04_PID m4M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S05_PID m5M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S06_PID m6M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S07_PID m7M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S08_PID m8M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S09_PID m9M : 16|8@1+ (1,0) [0|0] "" TOOL
- SG_ S0A_PID m10M : 16|8@1+ (1,0) [0|0] "" TOOL'''
+ SG_ RXACK : 12|4@1+ (1,0) [0|0] "" TOOL'''
 
-DBC_SG_MUL_VAL_HEADER='''SG_MUL_VAL_ 2024 S01_PID SERVICE 1-1;
-SG_MUL_VAL_ 2024 S02_PID SERVICE 2-2;
-SG_MUL_VAL_ 2024 S03_PID SERVICE 3-3;
-SG_MUL_VAL_ 2024 S04_PID SERVICE 4-4;
-SG_MUL_VAL_ 2024 S05_PID SERVICE 5-5;
-SG_MUL_VAL_ 2024 S06_PID SERVICE 6-6;
-SG_MUL_VAL_ 2024 S07_PID SERVICE 7-7;
-SG_MUL_VAL_ 2024 S08_PID SERVICE 8-8;
-SG_MUL_VAL_ 2024 S09_PID SERVICE 9-9;
-SG_MUL_VAL_ 2024 S0A_PID SERVICE 10-10;'''
+def val2str(v):
+    fstr = f"{float(v):.15f}"
+    if '.' not in fstr:
+        return fstr
+    return fstr.rstrip('0').rstrip('.')
+
+def gen_Sxx_PID():
+    SG_ = ""
+    SG_MUL_VAL_ = ""
+    for i in range(1, 11):
+        service = i
+        name = f"S{service:02X}_PID"
+        sg_ = f' SG_ {name} m{service}M : 16|8@1+ (1,0) [0|0] "" TOOL'
+        SG_ += f'{sg_}\n'
+        sg_mul_val = f'SG_MUL_VAL_ 2024 {name} SERVICE {service}-{service};'
+        SG_MUL_VAL_ += f'{sg_mul_val}\n'
+    return SG_, SG_MUL_VAL_
+
+def gen_sid():
+    SG_ = ""
+    SG_MUL_VAL_ = ""
+    for i in range(70):
+        service = i % 10 + 1
+        pid = 32 * (i // 10)
+        name = f'S{service:02X}_PID{pid:02X}_SupportedIDs_{pid + 1:02X}_{pid + 32:02X}'
+        sg_ = f' SG_ {name} m{pid} : 24|32@1+ (1,0) [0|0] "" TOOL'
+        SG_ += f'{sg_}\n'
+        sg_mul_val = f'SG_MUL_VAL_ 2024 {name} S{service:02X}_PID {pid}-{pid};'
+        SG_MUL_VAL_ += f'{sg_mul_val}\n'
+    return SG_, SG_MUL_VAL_
+
+def gen_pid(service, dfpid):
+    SG_ = ""
+    SG_MUL_VAL_ = ""
+    name_prefix = "" if service == 1 else f"S{service:02X}_"
+    for i, row in dfpid.iterrows():
+        if pd.isna(row['short_name']):
+            continue
+        mux_value = row['mux_value']
+        mux_value_trim = str(mux_value).replace("M","")
+
+        mux_name = f"{name_prefix}{row['mux_name']}" if not pd.isna(row['mux_name']) else f"S{service:02X}_PID"
+        name = f"{name_prefix}{row['short_name']}"
+
+        start = int(row['start'])
+        bits = int(row['bits'])
+        endian = int(row['endian'])
+        sign = row['sign']
+        scale = val2str(row['scale'])
+        unit = row['unit'] if not pd.isna(row['unit']) else ""
+
+        offset = val2str(row['offset'])
+        min = val2str(row['min'])
+        max = val2str(row['max'])
+        #  SG_ DTC_CNT m1 : 24|7@1+ (1,0) [0|0] "" TOOL
+
+        sg_ = f' SG_ {name} m{mux_value} : {start}|{bits}@{endian}{sign} ({scale},{offset}) [{min}|{max}] "{unit}" TOOL'
+        SG_ += f'{sg_}\n'
+
+        sg_mul_val = f'SG_MUL_VAL_ 2024 {name} {mux_name} {mux_value_trim}-{mux_value_trim};'
+        SG_MUL_VAL_ += f'{sg_mul_val}\n'
+    return SG_, SG_MUL_VAL_
 
 def gen(dbfile):
-    dfsid = pd.read_excel(dbfile, "dbcsid")
-    dfgen = pd.read_excel(dbfile, "dbcgen")
-    dfs = [dfsid, dfgen]
+    # dfsid = pd.read_excel(dbfile, "SID")
+    dfpid = pd.read_excel(dbfile, "PID")
+    dfitid = pd.read_excel(dbfile, "ITID")
+
+    SG_, SG_MUL_VAL_ = "", ""
+
+    # service & pid multiplexor
+    sg_, sg_mul_val_ = gen_Sxx_PID()
+    SG_ += sg_
+    SG_MUL_VAL_ += sg_mul_val_
+
+    # service 01 - 0A, Supported IDs
+    sg_, sg_mul_val_ = gen_sid()
+    SG_ += sg_
+    SG_MUL_VAL_ += sg_mul_val_
+
+    # service 01, Parameter IDs
+    sg_, sg_mul_val_ = gen_pid(1, dfpid)
+    SG_ += sg_
+    SG_MUL_VAL_ += sg_mul_val_
 
     print(DBCHEADER)
+    print(SG_)
+
+    print(SG_MUL_VAL_)
+
+    return
+
     index = 'SG_'
     for df in dfs:
         for i, row in df.iterrows():
@@ -143,5 +209,6 @@ if __name__ == '__main__':
         "check": check,
         "gen": gen,
         "tree": tree,
+        "test": gen_Sxx_PID,
     })
 
